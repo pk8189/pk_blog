5c8b9fbbf46762b2cb1d02db9889851ab5f2d993
pk8189 <pmkelly4444@gmail.com>
2019-07-28 21:18:14 -0400

    amsterdam-noord article

---
+++ src/routes/posts/2019-07-28-de_ceuvel/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Urban Development: De Ceuvel`,
+  tags: ['sustainability', 'amsterdam', 'biotech'],
+  spoiler: "A facinating sustainability project on an abandoned shipyard",
+  getContent: () => import('./document.mdx'),
+}
