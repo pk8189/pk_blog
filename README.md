09c3eaa552a1d47d1fe3af383aede9eaad1fb8bd
patrickmkelly <patrick.kelly@eigentech.com>
2019-11-17 12:21:27 +0000

    data filter in standard python article draft

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Deploying updates
 1. `git push` -> `master`
-2. `npm run deploy:prod`
+2. `npm run build`
+3. ignore the google adsense failure
+4. `npm run deploy:prod`
---
+++ src/routes/posts/2019-10-10-filtering_py_data/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Filtering data with Python`,
+  tags: ['software'],
+  spoiler: "There are many ways to do it. When handling big data, choose wisely.",
+  getContent: () => import('./document.mdx'),
+}
