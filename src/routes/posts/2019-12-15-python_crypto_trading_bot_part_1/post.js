685100851d9bd9495083df4794532f01e18a9832
pk8189 <pmkelly4444@gmail.com>
2019-12-15 13:26:59 +0000

    part 1 of 5 crypto bot in python with gemini series

---
+++ src/routes/posts/2019-12-15-python_crypto_trading_bot_part_1/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Building a python crypto trading bot for the Gemini Exchange: Part 1`,
+  tags: ['software'],
+  spoiler: "Building a python package from scratch with Poetry.",
+  getContent: () => import('./document.mdx'),
+}
