9b09eb553f793e5d382581c73ca343d662cddc08
patrickmkelly <patrick.kelly@eigentech.com>
2020-03-22 15:00:57 +0000

    remove crypto bot article, lost interest in this

--- src/routes/posts/2019-12-15-python_crypto_trading_bot_part_1/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Building a python crypto trading bot for the Gemini Exchange: Part 1`,
-  tags: ['software'],
-  spoiler: "Building a python package from scratch with Poetry.",
-  getContent: () => import('./document.mdx'),
-}
